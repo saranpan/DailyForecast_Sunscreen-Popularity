d14cb6c04cea232a42bee59cff09677b398c8d3e
Saran Pannasuriyaporn <69796499+wallik2@users.noreply.github.com>
2021-12-13 18:50:45 +0700

    Allow us to have daily popularity with long period
    Reference : http://alexdyachenko.com/all/how-to-get-daily-google-trends-data-for-any-period-with-r

---
+++ daily google trend scraper.R
@@ -0,0 +1,61 @@
+# Daily google trend scraper
+
+library(gtrendsR)
+library(tidyverse)
+library(lubridate)
+
+get_daily_gtrend <- function(keyword = c('Taylor Swift', 'Kim Kardashian'), geo = 'US', from = '2013-01-01', to = '2019-08-15') {
+  if (ymd(to) >= floor_date(Sys.Date(), 'month')) {
+    to <- floor_date(ymd(to), 'month') - days(1)
+    
+    if (to < from) {
+      stop("Specifying \'to\' date in the current month is not allowed")
+    }
+  }
+  
+  aggregated_data <- gtrends(keyword = keyword, geo = geo, time = paste(from, to))
+  if(is.null(aggregated_data$interest_over_time)) {
+    print('There is no data in Google Trends!')
+    return()
+  }
+  
+  mult_m <- aggregated_data$interest_over_time %>%
+    mutate(hits = as.integer(ifelse(hits == '<1', '0', hits))) %>%
+    group_by(month = floor_date(date, 'month'), keyword) %>%
+    summarise(hits = sum(hits)) %>%
+    ungroup() %>%
+    mutate(ym = format(month, '%Y-%m'),
+           mult = hits / max(hits)) %>%
+    select(month, ym, keyword, mult) %>%
+    as_tibble()
+  
+  pm <- tibble(s = seq(ymd(from), ymd(to), by = 'month'), 
+               e = seq(ymd(from), ymd(to), by = 'month') + months(1) - days(1))
+  
+  raw_trends_m <- tibble()
+  
+  for (i in seq(1, nrow(pm), 1)) {
+    curr <- gtrends(keyword, geo = geo, time = paste(pm$s[i], pm$e[i]))
+    if(is.null(curr$interest_over_time)) next
+    print(paste('for', pm$s[i], pm$e[i], 'retrieved', count(curr$interest_over_time), 'days of data (all keywords)'))
+    raw_trends_m <- rbind(raw_trends_m,
+                          curr$interest_over_time)
+  }
+  
+  trend_m <- raw_trends_m %>%
+    select(date, keyword, hits) %>%
+    mutate(ym = format(date, '%Y-%m'),
+           hits = as.integer(ifelse(hits == '<1', '0', hits))) %>%
+    as_tibble()
+  
+  trend_res <- trend_m %>%
+    left_join(mult_m) %>%
+    mutate(est_hits = hits * mult) %>%
+    select(date, keyword, est_hits) %>%
+    as_tibble() %>%
+    mutate(date = as.Date(date))
+  
+  return(trend_res)
+}
+
+tibble = get_daily_gtrend(keyword = c('¤ÃÕÁ¡Ñ¹á´´'), geo = 'US', from = '2013-01-01', to = '2013-09-01')
